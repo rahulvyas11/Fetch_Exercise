0cba29a384c5e3be1f12a08b848fc63c8af8503b
rahulvyas11 <rvyas@wisc.edu>
2024-05-13 11:16:07 -0500

    Initial Commit

---
+++ Fetch Take Home/Fetch_Take_HomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_Take_HomeApp.swift
+//  Fetch Take Home
+//
+//  Created by Rahul Vyas on 5/13/24.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_Take_HomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Fetch Take HomeUITests/Fetch_Take_HomeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Fetch_Take_HomeUITestsLaunchTests.swift
+//  Fetch Take HomeUITests
+//
+//  Created by Rahul Vyas on 5/13/24.
+//
+
+import XCTest
+
+final class Fetch_Take_HomeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
